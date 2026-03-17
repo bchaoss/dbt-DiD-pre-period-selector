01f836de0a8cdf0fca155b84b983c8559c86d37d
bchaoss <103650247+bchaoss@users.noreply.github.com>
2026-03-17 17:51:16 +0000

    chore: update devcontainer with notebook dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install dbt with postgres adapter
-pip install dbt-core dbt-postgres
+pip install dbt-core dbt-postgres matplotlib pandas
 sudo apt-get update && sudo apt-get install -y postgresql-client
 
 # Install package deps from integration_tests
